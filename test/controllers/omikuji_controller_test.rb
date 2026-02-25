d4c5d3df317b5978c2f970dfd1b7577fb9f3e2d2
Shoma <shoma.kaji@gmail.com>
2026-02-26 03:53:29 +0900

    Remove unused controller tests

--- test/controllers/omikuji_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class OmikujiControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get omikuji_index_url
-    assert_response :success
-  end
-end
--- test/controllers/pull_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class PullControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get pull_new_url
-    assert_response :success
-  end
-end
